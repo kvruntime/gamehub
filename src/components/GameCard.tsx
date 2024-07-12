14a550bf8c58241c0677719ff5cf3074d263f62b
kvruntime <kvruntime@gmail.com>
2024-07-12 02:32:16 +0000

    optimize-downloaded-image

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react';
 import { Game } from '../hooks/games-hook';
 import PlatformIcon from './PlatformIcon';
 import CriticScore from './CriticScore';
+import getCroppedImageUrl from '../services/image-url';
 
 interface Props {
 	game: Game;
@@ -10,7 +11,7 @@ export default function GameCard({ game }: Props) {
 	return (
 		<>
 			<Card borderRadius={'10px'} overflow={'hidden'}>
-				<Image src={game.background_image} />
+				<Image src={getCroppedImageUrl(game.background_image)} />
 				<CardBody>
 					<Heading fontSize={'2xl'}>{game.name}</Heading>
 					<HStack justifyContent={'space-between'}>
